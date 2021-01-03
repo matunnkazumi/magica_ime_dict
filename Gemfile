ed4dd6d7379b457b1bd75a0cad27e884057707c9
matunnkazumi <developer@matunnkazumi.space>
2021-01-04 00:47:17 +0900

    deep_symbolize_keys追加

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
+gem 'activesupport', '6.1.0'
 gem 'json_schemer', '0.2.17'
 gem 'minitest', '5.14.2'
 gem 'rubocop', '1.7', require: false
--- lib/dict/reader.rb
+++ lib/dict/reader.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 require 'yaml'
+require 'active_support/core_ext/hash'
 
 class Reader
   def self.load(path)
-    YAML.load_file(path.to_s)
+    YAML.load_file(path.to_s).map(&:deep_symbolize_keys)
   end
 end
