781a6fc5a5bcdd6745c98a690bf2d73b201c388f
matunnkazumi <developer@matunnkazumi.space>
2021-01-05 22:30:59 +0900

    重複除去のテストケース追加

--- spec/test_ime_atok.rb
+++ spec/test_ime_atok.rb
@@ -10,4 +10,31 @@ class TestImeATOK < Minitest::Test
     assert_equal '固有人他', ATOK::Entry.new('hoge', 'huga', :koyujinhoka).type_readable
     assert_equal '名詞', ATOK::Entry.new('hoge', 'huga', :meishi).type_readable
   end
+
+  def test_convert_uniqueness
+    # @type var src: Array[personal]
+    src = [
+      { sei: { yomi: 'aaa', kaki: 'bbb' }, mei: { yomi: 'ccc', kaki: 'ddd' } },
+      { sei: { yomi: 'aaa', kaki: 'bbb' }, mei: { yomi: 'eee', kaki: 'fff' } },
+      { sei: { yomi: 'eee', kaki: 'fff' }, mei: { yomi: 'ccc', kaki: 'ddd' } }
+    ]
+
+    result = ATOK.convert(src)
+
+    assert_convert_uniqueness result, 'aaa', 'bbb', :koyujinsei
+    assert_convert_uniqueness result, 'ccc', 'ddd', :koyujinmei
+    assert_convert_uniqueness result, 'eee', 'fff', :koyujinsei
+    assert_convert_uniqueness result, 'eee', 'fff', :koyujinmei
+  end
+
+  private
+
+  def assert_convert_uniqueness(list, yomi, kaki, category)
+    f = method(:is_same).curry.call(yomi, kaki, category)
+    assert_equal 1, list.select(&f).count, "#{yomi} #{kaki} #{category}"
+  end
+
+  def is_same(yomi, kaki, type, entry)
+    entry.yomi == yomi && entry.kaki == kaki && entry.type == type
+  end
 end
--- spec/test_ime_ms.rb
+++ spec/test_ime_ms.rb
@@ -10,4 +10,31 @@ class TestImeMS < Minitest::Test
     assert_equal '人名', MSIME::Entry.new('hoge', 'huga', :jinmei).type_readable
     assert_equal '名詞', MSIME::Entry.new('hoge', 'huga', :meishi).type_readable
   end
+
+  def test_convert_uniqueness
+    # @type var src: Array[personal]
+    src = [
+      { sei: { yomi: 'aaa', kaki: 'bbb' }, mei: { yomi: 'ccc', kaki: 'ddd' } },
+      { sei: { yomi: 'aaa', kaki: 'bbb' }, mei: { yomi: 'eee', kaki: 'fff' } },
+      { sei: { yomi: 'eee', kaki: 'fff' }, mei: { yomi: 'ccc', kaki: 'ddd' } }
+    ]
+
+    result = MSIME.convert(src)
+
+    assert_convert_uniqueness result, 'aaa', 'bbb', :sei
+    assert_convert_uniqueness result, 'ccc', 'ddd', :mei
+    assert_convert_uniqueness result, 'eee', 'fff', :sei
+    assert_convert_uniqueness result, 'eee', 'fff', :mei
+  end
+
+  private
+
+  def assert_convert_uniqueness(list, yomi, kaki, category)
+    f = method(:is_same).curry.call(yomi, kaki, category)
+    assert_equal 1, list.select(&f).count, "#{yomi} #{kaki} #{category}"
+  end
+
+  def is_same(yomi, kaki, type, entry)
+    entry.yomi == yomi && entry.kaki == kaki && entry.type == type
+  end
 end
