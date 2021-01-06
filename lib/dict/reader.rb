2381ca6c0d50a92bd7e699aa64aaafb3e3678106
matunnkazumi <developer@matunnkazumi.space>
2021-01-06 21:57:12 +0900

    HashのキーをSymbolにする方法を変更
    * load_fileの引数でできる

--- lib/dict/reader.rb
+++ lib/dict/reader.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 require 'yaml'
-require 'active_support/core_ext/hash'
 
 class Reader
   def self.load(path)
-    YAML.load_file(path.to_s).map(&:deep_symbolize_keys)
+    YAML.load_file(path.to_s, symbolize_names: true)
   end
 end
