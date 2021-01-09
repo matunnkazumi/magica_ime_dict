c48cc93e345f0df484b3a9991978a8c9126a821b
matunnkazumi <developer@matunnkazumi.space>
2021-01-09 15:32:09 +0900

    テストケース追加
    ・読みと書きが同じやつは除去
    ・ファイルごとテスト

--- spec/ime/test_skk.rb
+++ spec/ime/test_skk.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require 'tempfile'
 require 'minitest/autorun'
 require_relative '../../lib/ime/skk'
+require_relative '../../lib/dict/reader'
 
 class TestImeSKK < Minitest::Test
-  def test_convert_category_mapping
+  def test_convert
     # @type var src: Array[personal]
     src = [{
       sei: { yomi: 'aaa', kaki: 'bbb' },
@@ -14,10 +16,25 @@ class TestImeSKK < Minitest::Test
 
     result = IME::SKK.convert(src)
 
-    assert_convert_mapping result, 'aaa', 'bbb'
-    assert_convert_mapping result, 'ccc', 'ddd'
-    assert_convert_mapping result, 'eee', 'fff'
-    assert_convert_mapping result, 'ggg', 'hhh'
+    assert_convert_exist result, 'aaa', 'bbb'
+    assert_convert_exist result, 'ccc', 'ddd'
+    assert_convert_exist result, 'eee', 'fff'
+    assert_convert_exist result, 'ggg', 'hhh'
+  end
+
+  def test_convert_remove_same_entry
+    # @type var src: Array[personal]
+    src = [{
+      sei: { yomi: 'aaa', kaki: 'aaa' },
+      mei: { yomi: 'ccc', kaki: 'ccc' },
+      sonota: [{ yomi: 'eee', kaki: 'eee' }]
+    }]
+
+    result = IME::SKK.convert(src)
+
+    refute_convert_exist result, 'aaa', 'aaa'
+    refute_convert_exist result, 'ccc', 'ccc'
+    refute_convert_exist result, 'eee', 'eee'
   end
 
   def test_convert_uniqueness
@@ -35,11 +52,49 @@ class TestImeSKK < Minitest::Test
     assert_convert_uniqueness result, 'eee', 'fff'
   end
 
+  TEST_WRITEFILE_SOURCE = <<~TESTFILE
+    - sei:
+        yomi: aaa
+        kaki: bbb
+      mei:
+        yomi: ccc
+        kaki: ddd
+      sonota:
+        - yomi: eee
+          kaki: fff
+    - sei:
+        yomi: aaa
+        kaki: bbb
+    - sei:
+        yomi: aaa
+        kaki: ccc
+  TESTFILE
+
+  TEST_WRITEFILE_EXPECTED = <<~TESTFILE
+    ;; okuri-nasi entries.
+    aaa /bbb/ccc/
+    ccc /ddd/
+    eee /fff/
+  TESTFILE
+
+  def test_writefile
+    file_check(TEST_WRITEFILE_SOURCE, TEST_WRITEFILE_EXPECTED) do |yaml, dest|
+      src = Reader.load(yaml)
+      result = IME::SKK.convert(src)
+      IME::SKK.write_file(result, dest)
+    end
+  end
+
   private
 
-  def assert_convert_mapping(list, yomi, kaki)
+  def assert_convert_exist(list, yomi, kaki)
     f = method(:is_same).curry.call(yomi, kaki)
     assert list.any?(&f), "#{yomi} #{kaki}"
+  end
+
+  def refute_convert_exist(list, yomi, kaki)
+    f = method(:is_same).curry.call(yomi, kaki)
+    refute list.any?(&f), "#{yomi} #{kaki}"
   end
 
   def assert_convert_uniqueness(list, yomi, kaki)
@@ -49,5 +104,20 @@ class TestImeSKK < Minitest::Test
 
   def is_same(yomi, kaki, entry)
     entry.yomi == yomi && entry.kaki == kaki
+  end
+
+  def file_check(source, expected)
+    tmp_src = Tempfile.open('temp_source') do |fp|
+      fp.write source
+      fp
+    end
+    tmp_result = Tempfile.open('temp_result')
+
+    yield Pathname.new(tmp_src.path), Pathname.new(tmp_result.path)
+
+    assert_equal expected, tmp_result.read
+
+    tmp_result.close
+    tmp_src.close
   end
 end
