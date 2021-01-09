bfe8375bc27faeb90bd9f21bed1d482db42064ad
matunnkazumi <developer@matunnkazumi.space>
2021-01-09 15:47:53 +0900

    型

--- lib/ime/skk.rb
+++ lib/ime/skk.rb
@@ -6,27 +6,15 @@ require_relative './collection'
 
 module IME
   module SKK
-    class Entry2 < ::IME::Collection::Entry
-      def initialize(yomi, kaki)
-        super(yomi, kaki, nil)
-      end
-
-      def identical?
-        yomi == kaki
-      end
-    end
-
-    module_function
-
-    def convert(src)
+    def self.convert(src)
       src
         .map { |entry| convert_person(entry) }
         .flatten
-        .reject(&:identical?)
+        .reject { |entry| entry.yomi == entry.kaki }
         .uniq
     end
 
-    def write_file(src, path)
+    def self.write_file(src, path)
       path.open('wb') do |file|
         file.puts ';; okuri-nasi entries.'
 
@@ -40,7 +28,7 @@ module IME
       end
     end
 
-    def convert_person(person)
+    def self.convert_person(person)
       result = []
 
       sei = pair_to_entry(person[:sei])
@@ -55,13 +43,13 @@ module IME
       result
     end
 
-    def pair_to_entry(pair)
+    def self.pair_to_entry(pair)
       return if pair.nil?
 
       kaki = pair[:kaki]
       yomi = pair[:yomi]
 
-      Entry2.new(yomi, kaki) unless yomi.nil?
+      ::IME::Collection::Entry.new(yomi, kaki, nil) unless yomi.nil?
     end
   end
 end
